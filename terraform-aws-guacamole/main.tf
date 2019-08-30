45c94ef815d8e876fe01f6f49e56928608418736
Brian Muhlenkamp <29447475+moskey71@users.noreply.github.com>
2019-08-30 17:10:32 -0400

    add guac module

---
+++ terraform-aws-guacamole/main.tf
@@ -0,0 +1,51 @@
+resource "aws_cloudformation_stack" "watchmaker-lx-instance" {
+  template_body = "${file("${path.module}/ra_guac_autoscale_public_alb.template.cfn.yaml")}"
+
+  name               = "${var.Name}"
+  capabilities       = "${var.Capabilities}"
+  disable_rollback   = "${var.DisableRollback}"
+  iam_role_arn       = "${var.IamRoleArn}"
+  notification_arns  = "${var.NotificationArns}"
+  on_failure         = "${var.OnFailureAction}"
+  policy_body        = "${var.PolicyBody}"
+  policy_url         = "${var.PolicyUrl}"
+  tags               = "${var.StackTags}"
+  timeout_in_minutes = "${var.TimeoutInMinutes}"
+
+  parameters = {
+"AmiId" = "${var.AmiId}"
+"AmiNameSearchString" = "${var.AmiNameSearchString}"
+"BrandText" = "${var.BrandText}"
+"CloudWatchAgentUrl" = "${var.CloudWatchAgentUrl}"
+"DesiredCapacity" = "${var.DesiredCapacity}"
+"ForceUpdateToggle" = "${var.ForceUpdateToggle}"
+"GuacBaseDN" = "${var.GuacBaseDN}"
+"GuacamoleVersion" = "${var.GuacamoleVersion}"
+"GuacdVersion" = "${var.GuacdVersion}"
+"InstanceType" = "${var.InstanceType}"
+"KeyPairName" = "${var.KeyPairName}"
+"LdapDN" = "${var.LdapDN}"
+"LdapServer" = "${var.LdapServer}"
+"MaxCapacity" = "${var.MaxCapacity}"
+"MinCapacity" = "${var.MinCapacity}"
+"PrivateSubnetIDs" = "${var.PrivateSubnetIDs}"
+"PublicSubnetIDs" = "${var.PublicSubnetIDs}"
+"ScaleDownDesiredCapacity" = "${var.ScaleDownDesiredCapacity}"
+"ScaleDownSchedule" = "${var.ScaleDownSchedule}"
+"ScaleUpSchedule" = "${var.ScaleUpSchedule}"
+"SslCertificateName" = "${var.SslCertificateName}"
+"SslCertificateService" = "${var.SslCertificateService}"
+"URL1" = "${var.URL1}"
+"URL2" = "${var.URL2}"
+"URLText1" = "${var.URLText1}"
+"URLText2" = "${var.URLText2}"
+"UpdateSchedule" = "${var.UpdateSchedule}"
+"VPC" = "${var.VPC}"
+  }
+
+  timeouts {
+    create = "${var.TimeoutInMinutes}m"
+    delete = "${var.TimeoutInMinutes}m"
+    update = "${var.TimeoutInMinutes}m"
+  }
+}
---
+++ terraform-aws-guacamole/variables.tf
@@ -0,0 +1,106 @@
+variable "AmiId" {
+  default = ""
+  description = ""
+  }
+variable "AmiNameSearchString" {
+  default = "amzn-ami-hvm-2018.03.*-x86_64-gp2"
+  description = ""
+  }
+variable "BrandText" {
+  default = "Remote Access"
+  description = ""
+  }
+variable "CloudWatchAgentUrl" {
+  default = "s3://amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm"
+  description = ""
+  }
+variable "DesiredCapacity" {
+  default = "1"
+  description = ""
+  }
+variable "ForceUpdateToggle" {
+  default = "B"
+  description = ""
+  }
+variable "GuacBaseDN" {
+  default = "CN=GuacConfigGroups"
+  description = ""
+  }
+variable "GuacamoleVersion" {
+  default = "1.0.0"
+  description = ""
+  }
+variable "GuacdVersion" {
+  default = "1.0.0"
+  description = ""
+  }
+variable "InstanceType" {
+  default = "c5.large"
+  description = ""
+  }
+variable "KeyPairName" {
+  default = ""
+  description = ""
+  }
+variable "LdapDN" {
+  description = "DC=domain,DC=com"
+  }
+variable "LdapServer" {
+  description = "domain.com"
+  }
+variable "MaxCapacity" {
+  default = "1"
+  description = ""
+  }
+variable "MinCapacity" {
+  default = "0"
+  description = ""
+  }
+variable "PrivateSubnetIDs" {
+  description = "Comma separated string of subnets"
+  }
+variable "PublicSubnetIDs" {
+  description = "Comma separated string of subnets"
+  }
+variable "ScaleDownDesiredCapacity" {
+  default = "1"
+  description = ""
+  }
+variable "ScaleDownSchedule" {
+  default = ""
+  description = ""
+  }
+variable "ScaleUpSchedule" {
+  default = ""
+  description = ""
+  }
+variable "SslCertificateName" {
+  description = "Name of AWS ACM Certificate"
+  }
+variable "SslCertificateService" {
+  default = "ACM"
+  description = ""
+  }
+variable "URL1" {
+  default = "https://accounts.domain.com"
+  description = ""
+  }
+variable "URL2" {
+  default = "https://redmine.domain.com"
+  description = ""
+  }
+variable "URLText1" {
+  default = "Account Services"
+  description = ""
+  }
+variable "URLText2" {
+  default = "Redmine"
+  description = ""
+  }
+variable "UpdateSchedule" {
+  default = "cron(0 5 ? * Sun *)"
+  description = ""
+  }
+variable "VPC" {
+  description = "AWS VPC"
+  }
